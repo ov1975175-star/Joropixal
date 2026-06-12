d316e17e88f4ab4029500c81d0a0469f83712d5a
ov1975175-star <ov1975175@gmail.com>
2026-06-13 00:12:19 +0530

    Delete bot directory

--- bot/handlers/user.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-
-router = Router()
-
-@router.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("Bot is working perfectly!")
-    
