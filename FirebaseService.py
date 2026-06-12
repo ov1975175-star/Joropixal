b47fef7ff1d1f6eba0bbc5851dd2ba8115392895
ov1975175-star <ov1975175@gmail.com>
2026-06-12 22:59:30 +0530

    Update FirebaseService.py

--- FirebaseService.py
+++ FirebaseService.py
@@ -1,7 +1,15 @@
 import json
 import os
-from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import credentials, db
 
-# Environment variable se json uthayein
+# Environment variable se JSON load karein
 json_data = json.loads(os.getenv("FIREBASE_JSON"))
 cred = credentials.Certificate(json_data)
+# Connection Initialize
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://joro-gaming-default-rtdb.firebaseio.com'})
+
+class FirebaseService:
+    def update_order(self, order_id, data):
+        db.reference(f'orders/{order_id}').update(data)
+      
