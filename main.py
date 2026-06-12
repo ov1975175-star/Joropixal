0b4d3425a25f0869df05d7860dd6f29c73452a8b
ov1975175-star <ov1975175@gmail.com>
2026-06-12 21:10:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-# main.py mein
-from bot.handlers import user_router, admin_router
+import asyncio
+from aiogram import Bot, Dispatcher
+from bot.handlers.user import router as user_router
+from bot.handlers.admin import router as admin_router
 
-# Dono ka naam alag hai toh include_router sahi se chalega
-dp.include_router(user_router)
-dp.include_router(admin_router)
+async def main():
+    bot = Bot(token="8001535871:AAEr-DvtKgP3XggXNqih-rzy1Yfjx4UqAhI")
+    dp = Dispatcher()
+
+    dp.include_router(user_router)
+    dp.include_router(admin_router)
+
+    print("Bot is running...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+  
