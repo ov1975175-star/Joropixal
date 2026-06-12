5eb0c6c72efd143f9d498f080a671592a021a764
ov1975175-star <ov1975175@gmail.com>
2026-06-12 21:50:56 +0530

    Update FirebaseService.py

--- FirebaseService.py
+++ FirebaseService.py
@@ -1,4 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+# serviceAccountKey.json aapke folder mein hona chahiye
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://joro-gaming-default-rtdb.firebaseio.com' 
+})
+
+class FirebaseService:
+    def get_order(self, order_id):
+        return db.reference(f'orders/{order_id}').get()
+
+    def update_order(self, order_id, data):
+        db.reference(f'orders/{order_id}').update(data)
+        
     def get_products(self):
-        # Database mein 'products' node se saara data uthaye ga
+        # Yeh function products list karega
         return db.reference('products').get()
         
